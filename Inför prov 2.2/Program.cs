1604ca35f1d3da2ab9acdbbd0f0c428723f7f536
maol07001 <maol07001@E-PW05CGR4>
2024-10-03 14:12:36 +0200

    Add project files.

---
+++ Inför prov 2.2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace uppgift_2._2
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Elin hur långt håppade du i meter?");
+            string elin = Console.ReadLine();
+            double Elin = double.Parse(elin);
+            Console.WriteLine("Alma hur långt hoppade du i meter?");
+            string alma = Console.ReadLine();
+            double Alma = double.Parse(alma);
+            double skilnad = Elin-Alma;
+            Console.WriteLine("Elin hoppade" +skilnad+ "meter längre än Alma");
+        }
+    }
+}
